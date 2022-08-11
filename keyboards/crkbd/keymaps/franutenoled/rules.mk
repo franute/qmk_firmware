16c10b30c954d82d93f5d44d396bd1bf31f5232c
Francisco Torres Pérez <259762-franute@users.noreply.gitlab.com>
2022-08-12 00:35:10 +0200

    Reorganize corne alt keys, minor adjustments. Adding extra corne keymap for the choc.

---
+++ keyboards/crkbd/keymaps/franutenoled/rules.mk
@@ -0,0 +1,19 @@
+OLED_ENABLE 		= yes
+OLED_DRIVER 		= SSD1306
+TAP_DANCE_ENABLE 	= no
+EXTRAKEY_ENABLE 	= yes
+NKRO_ENABLE        	= yes
+CONSOLE_ENABLE		= no
+
+# Fancy Stuff
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = no
+OLED_DRIVER_ENABLE = yes
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE   = no
+
+EXTRAFLAGS        += -flto
+COMMAND_ENABLE	   = no # Commands for debug and configuration
+UNICODE_ENABLE     = no# Unicode
+MOUSEKEY_ENABLE    = no # Mouse keys(+4700)
